a4569a48f303e2d6bdafddd3975ad975f2957e77
jaechang1502 <jaechang92@naver.com>
2022-04-28 19:09:25 +0900

    업로드 추가

--- ketchup/src/views/LeftSideMenu/Upload/Upload.vue
+++ ketchup/src/views/LeftSideMenu/Upload/Upload.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <b-button class="w-100" style="background-color: #ff7800"> 내 파일 업로드</b-button>
-    <label for="upload" class="w-100 d-flex">
-        <b-icon icon="search"></b-icon>
-         <b-form-input  placeholder="통합검색" class="total_search"></b-form-input>
-    </label>
+    <b-button class="w-100 mb-2" style="background-color: #ff7800"> 내 파일 업로드</b-button>
+    <b-input-group class="mb-3">
+            <b-input-group-prepend is-text="is-text">
+                <b-icon icon="search"></b-icon>
+            </b-input-group-prepend>
+            <b-form-input v-model="text" placeholder="통합검색" class="total_search"></b-form-input>
+        </b-input-group>
+        <div>
+          <!---내일할것--->
+        </div>
   </div>
   
 </template>
--- ketchup/src/views/SideMenu.vue
+++ ketchup/src/views/SideMenu.vue
@@ -3,13 +3,13 @@
   <div class="d-flex flex-column vh-70 flex-shrink-0 p-1 text-white bg-dark" style="width: 75px;"> 
      
     <ul class="nav nav-pills flex-column mb-auto">
-        <li @click="$store.commit('mainmenu',0)"> <a href="#" class="nav-link text-white text-center h6 p-2" style=" font-size:10.5px"><b-icon icon="search" animation="cylon" font-scale="2" shift-v="2"></b-icon><br><p>통합 검색</p></a></li>
-        <li @click="$store.commit('mainmenu',1)"> <a href="#" class="nav-link text-white text-center h6 p-3" style=" font-size:10.5px"><b-icon icon="emoji-wink" animation="fade" font-scale="2" shift-v="2"></b-icon><br>캐릭터</a></li>
-        <li  @click="$store.commit('mainmenu',2)"> <a href="#" class="nav-link text-white text-center h6 p-3" style="font-size:10.5px"><b-icon icon="fonts" animation="spin" font-scale="2" shift-v="2"></b-icon><br>텍스트</a></li>
-        <li  @click="$store.commit('mainmenu',3)"> <a href="#" class="nav-link text-white text-center h6 p-3" style="font-size:10.5px"><b-icon icon="chat" animation="throb" font-scale="2" shift-v="2"></b-icon><br>말풍선</a></li>
-        <li  @click="$store.commit('mainmenu',4)"> <a href="#" class="nav-link text-white text-center h6 p-3" style="font-size:10.5px"><b-icon icon="grid" animation="cylon" font-scale="2" shift-v="2"></b-icon><br>조합</a></li>
-        <li  @click="$store.commit('mainmenu',5)"> <a href="#" class="nav-link text-white text-center h6 p-3" style="font-size:10.5px"><b-icon icon="card-image" animation="fade" font-scale="2" shift-v="2"></b-icon><br>배경</a></li>
-        <li  @click="$store.commit('mainmenu',6)"> <a href="#" class="nav-link text-white text-center h6 p-3" style="font-size:10.5px"><b-icon icon="upload" animation="throb" font-scale="2" shift-v="2"></b-icon><br>업로드</a></li>
+        <li @click="$store.commit('mainmenu',0)"> <a href="#" class="nav-link text-white text-center h6 p-2" style=" font-size:10.5px"><b-icon icon="search"  font-scale="2" shift-v="2"></b-icon><br><p>통합 검색</p></a></li>
+        <li @click="$store.commit('mainmenu',1)"> <a href="#" class="nav-link text-white text-center h6 p-3" style=" font-size:10.5px"><b-icon icon="emoji-wink"  font-scale="2" shift-v="2"></b-icon><br>캐릭터</a></li>
+        <li  @click="$store.commit('mainmenu',2)"> <a href="#" class="nav-link text-white text-center h6 p-3" style="font-size:10.5px"><b-icon icon="fonts"  font-scale="2" shift-v="2"></b-icon><br>텍스트</a></li>
+        <li  @click="$store.commit('mainmenu',3)"> <a href="#" class="nav-link text-white text-center h6 p-3" style="font-size:10.5px"><b-icon icon="chat"  font-scale="2" shift-v="2"></b-icon><br>말풍선</a></li>
+        <li  @click="$store.commit('mainmenu',4)"> <a href="#" class="nav-link text-white text-center h6 p-3" style="font-size:10.5px"><b-icon icon="grid"  font-scale="2" shift-v="2"></b-icon><br>조합</a></li>
+        <li  @click="$store.commit('mainmenu',5)"> <a href="#" class="nav-link text-white text-center h6 p-3" style="font-size:10.5px"><b-icon icon="card-image"  font-scale="2" shift-v="2"></b-icon><br>배경</a></li>
+        <li  @click="$store.commit('mainmenu',6)"> <a href="#" class="nav-link text-white text-center h6 p-3" style="font-size:10.5px"><b-icon icon="upload"  font-scale="2" shift-v="2"></b-icon><br>업로드</a></li>
     </ul>   
 </div>
 <div class="d-flex flex-column vh-70  p-1 text-white bg-dark " style="width: 320px;">
