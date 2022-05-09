da49f0463e2e4f33c66e35094fdf2da164025bd6
jaechang1502 <jaechang92@naver.com>
2022-05-09 14:20:25 +0900

    TypeScript intellisense is disabled on template, you can config `"jsx": "preserve"` 오류

--- ketchup/src/router/router.js
+++ ketchup/src/router/router.js
@@ -1,15 +1,21 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "../views/editor/Home";
+import Main from "../views/Home/HomeMain";
 Vue.use(Router);
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
+      path: "/editor",
       name: "home",
       component: Home
+    },
+    {
+      path: "/",
+      name: "main",
+      component: Main
     }
   ]
 });
---
+++ ketchup/src/views/Home/HomeMain.vue
@@ -0,0 +1,16 @@
+<template>
+<mainhead></mainhead>
+</template>
+
+<script>
+import mainhead from './Head/MainHead.vue'
+export default {
+    components: {
+        mainhead,
+    }
+}
+</script>
+
+<style>
+
+</style>
