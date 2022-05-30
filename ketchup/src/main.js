1550c9a5fcf60a817bea41894cdd351dc2565fc5
jaechang1502 <jaechang92@naver.com>
2022-05-30 15:28:20 +0900

    모듈 변경과 firebasestore 오류걸림

--- ketchup/src/main.js
+++ ketchup/src/main.js
@@ -1,19 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/router'
-import {BootstrapVue,BootstrapVueIcons} from "bootstrap-vue";
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap-vue/dist/bootstrap-vue.css";
+import './plugins/Vuebootstrap'
 import "tui-image-editor/dist/tui-image-editor.css";
 import carousel from 'vue-owl-carousel'
 import store from './store/store.js';
-import './etc/tool.scss';
-import './etc/tool.css';
-import './etc/fontdata.scss'
+import './plugins/cssdata'
+import './plugins/firebase'
 
 
-Vue.use(BootstrapVue);
-Vue.use(BootstrapVueIcons);
+
 Vue.use(carousel);
 
 Vue.config.productionTip = false
---
+++ ketchup/src/plugins/cssdata.js
@@ -0,0 +1,4 @@
+
+import '../etc/tool.scss';
+import '../etc/tool.css';
+import '../etc/fontdata.scss'
